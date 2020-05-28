efe52c217ec37e71b20852d76138295354ab2ec8
Pavel Maletin <maletinchess>
2020-05-28 21:55:32 +0700

    init

---
+++ test.js
@@ -0,0 +1,7 @@
+var demo = require('study4');
+demo.printMsg();
+var x = require('lodash');
+console.log(x.uniq([1, 1, 2]));
+console.log(x.uniq(['k', 'l', 'k']));
+console.log([1, 2, 3].map((n) => Math.sqrt(n)));
+
